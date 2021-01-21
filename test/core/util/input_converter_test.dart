44a5e81528640004d77bbda4db14baacdd4b9f34
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2021-01-21 16:19:47 +0100

    Added input coverter and test

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,46 @@
+import 'package:clean_architecture/core/util/input_converter.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInteger', () {
+    test(
+        'should return an integer when the string represents an unsigned integer',
+        () {
+      // arrange
+      final str = '123';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Right(123));
+    });
+
+    test(
+        'should return an Failure when the string is not an integer',
+        () {
+      // arrange
+      final str = 'abc';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+
+    test(
+        'should return an Failure when the string is a negative integer',
+        () {
+      // arrange
+      final str = '-123';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+  });
+}
